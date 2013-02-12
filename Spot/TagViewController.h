c8a311ae7a9b0855668653b0547943f9dd8a21c0
Grant Mathews <johnfn@gmail.com>
2013-02-11 21:10:09 -0800

    BOOM. transition.

---
+++ Spot/TagViewController.h
@@ -0,0 +1,13 @@
+//
+//  TagViewController.h
+//  Spot
+//
+//  Created by Grant Mathews on 2/11/13.
+//  Copyright (c) 2013 johnfn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagViewController : UITableViewController
+
+@end
